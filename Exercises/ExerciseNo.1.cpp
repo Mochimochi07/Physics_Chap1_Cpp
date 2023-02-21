41abacdc6582a8bc181eb5ca42a708c1147266af
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-02-21 17:42:33 +0800

    Update ExerciseNo.1.cpp

--- Exercises/ExerciseNo.1.cpp
+++ Exercises/ExerciseNo.1.cpp
@@ -1,1 +1,17 @@
 //How many nanoseconds does it take light to travel 1.00m in a vacuum?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  double spdoflight = 299792458;
+  double s = 1;
+  double time;
+  
+  time = s / spdoflight ;
+  
+  cout << time;
+  return 0;
+}
+
